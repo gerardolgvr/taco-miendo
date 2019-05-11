7cc3f4923da34d0264eee68cef5d5d5ae04d2000
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-05-10 23:52:16 -0500

    Creating fragments

---
+++ app/src/main/java/com/example/tacomiendo/Fragments/ThirdFragment.java
@@ -0,0 +1,28 @@
+package com.example.tacomiendo.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tacomiendo.R;
+
+
+public class ThirdFragment extends Fragment {
+
+    public ThirdFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+
+        return view;
+    }
+
+}
