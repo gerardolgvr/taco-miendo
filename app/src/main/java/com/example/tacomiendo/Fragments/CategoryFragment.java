a12c508f894fedb81c21869b572158bf748d71c4
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-05-11 21:10:04 -0500

    Adding nav drawer fragments

---
+++ app/src/main/java/com/example/tacomiendo/Fragments/CategoryFragment.java
@@ -0,0 +1,28 @@
+package com.example.tacomiendo.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tacomiendo.R;
+
+public class CategoryFragment extends Fragment {
+
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        return view;
+    }
+
+}
