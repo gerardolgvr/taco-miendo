49023baceff6cc7c8a703d97c88dcab687913219
Gerardo Vázquez <gerardolgvr0310@gmail.com>
2019-05-11 21:10:49 -0500

    Implementing nav drawer at MainActivity

--- app/src/main/java/com/example/tacomiendo/Activities/MainActivity.java
+++ app/src/main/java/com/example/tacomiendo/Activities/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.tacomiendo.Activities;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.tacomiendo.Adapters.PagerAdapter;
 import com.example.tacomiendo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
+        //setting the toolbar
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
@@ -49,10 +57,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    //setting up the toolbar
+    public void setToolbar(){
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                //open the navigationDrawer
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
